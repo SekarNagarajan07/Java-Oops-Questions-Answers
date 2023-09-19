81bffc66158a62c89834333ef23e3d161f23a9c3
sekar N <sekarnagarajan1234@gmail.com>
2023-09-19 20:46:07 +0530

    java

--- Class - Objects/src/Prorgam1.java
+++ Class - Objects/src/Prorgam1.java
@@ -1,5 +1,26 @@
-public class Prorgam1 {
+/*1.Create a class for Calculator. Write methods for finding sum, difference,
+product and quotient. All methods should take 2 numbers as input and
+return one integer.*/
+class Calculator {
+    public void add(int num1, int num2){
+        System.out.println("Total: " + (num1 + num2));
+    }
+    public void diff(int num1, int num2){
+        System.out.println("Different: " + (num1-num2));
+    }
+    public void product(int num1, int num2){
+        System.out.println("product: " + (num1 * num2));
+    }
+    public void quotient(int num1, int num2){
+        System.out.println("Quotient: " + (num1/num2));
+    }
+}
+class Program1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calculator c = new Calculator();
+        c.add(10,20);
+        c.diff(20, 10);
+        c.product(20,10);
+        c.quotient(20,10);
     }
 }
