3074f11c4af785cee3c8b07a63fa19b1e0fd2781
sekar N <sekarnagarajan1234@gmail.com>
2023-09-19 22:41:48 +0530

    java

---
+++ Class - Objects/src/Program3.java
@@ -0,0 +1,12 @@
+/*3. Create class named Student. Write methods to read marks as input and
+return total as output. Another method to take total and number of subject
+as input and return average.*/
+
+class Program3 {
+     public static void main(String[] args) {
+
+     }
+}
+class Student{
+    
+}
