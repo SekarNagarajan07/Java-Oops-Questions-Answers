3e45df766d6496af534192797992bf1084858f9b
sekar N <sekarnagarajan1234@gmail.com>
2023-09-30 22:25:15 +0530

    java

--- Abstraction/src/Program1.java
+++ Abstraction/src/Program1.java
@@ -1,5 +1,33 @@
- class Program1 {
+abstract class Bank {
+    abstract  int getBalance();
+}
+class BankA extends Bank {
+    private int balance = 100;
+    int getBalance(){
+        return balance;
+    }
+}
+
+class BankB extends Bank {
+    private int balance = 150;
+    int getBalance(){
+        return balance;
+    }
+}
+class BankC extends Bank {
+    private int balance = 200;
+    int getBalance(){
+        return balance;
+    }
+}
+class Program1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BankA bankA = new BankA();
+        BankB bankB = new BankB();
+        BankC bankC = new BankC();
+
+        System.out.println(bankA.getBalance());
+        System.out.println(bankB.getBalance());
+        System.out.println(bankC.getBalance());
     }
 }
