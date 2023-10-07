d0a838806632f7d7798068f033f83907ab4078ea
sekar N <sekarnagarajan1234@gmail.com>
2023-10-07 11:24:30 +0530

    java

---
+++ Abstraction/src/Program3.java
@@ -0,0 +1,52 @@
+abstract class Shape {
+    public abstract void printArea();
+}
+
+class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+
+    public void printArea(){
+        double area = length * width;
+        System.out.println("Area of Rectangle: " + area);
+    }
+}
+
+class Triangle extends Shape {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height){
+        this.base = base;
+        this.height = height;
+    }
+
+    public void printArea(){
+        double area = 0.5 * base * height;
+        System.out.println("Area of Triangle: " + area);
+    }
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public void printArea(){
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of Circle: " + area);
+    }
+}
+
+class Program3 {
+    public static void main(String[] args) {
+
+    }
+}
